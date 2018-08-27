b504725f6f5dcca7719fb2a9c425173d42e27ade
Rider <riderjensen@gmail.com>
2018-08-27 15:44:31 -0600

    Fixing delete issue

--- displayEmail/deleteEmails.php
+++ displayEmail/deleteEmails.php
@@ -30,14 +30,15 @@
                     <div class="col-md-4"></div>
                     <div class="col-md-4">
                         <h1>Delete emails from the database</h1>
-                            <form action="<?php $SERVER['PHP_SELF'];?>" method="POST" enctype="multipart/form-data">
+                            <form action="deleteEmails.php" method="GET" enctype="multipart/form-data">
                                 
 <?php
-$dbconnection = mysqli_connect('localhost','test','','test') or die ('connection failed');
+
+$dbconnection = mysqli_connect('localhost','riderjen_3760usr','Ilikecheese3!','riderjen_3760test') or die ('connection failed');
 
 // delete the code
-if (isset($_POST['submit'])) {
-    foreach($_POST['emailArray' as $deleteId]){
+if (isset($_GET['submit'])) {
+    foreach($_GET['emailArray'] as $deleteId){
         $myQuery = "DELETE FROM newsletter WHERE id=$deleteId";
         $result = mysqli_query($dbconnection, $myQuery) or die ('query failed');
     }; //end for each
@@ -51,7 +52,7 @@ $result = mysqli_query($dbconnection, $query) or die ('query failed');
 // display records
 while($row = mysqli_fetch_array($result)) {
     echo '<label>';
-    echo '<div class="form-group"><input type="chexbox" class="form-control" value="'.$row['id'].'" name="emailArray[]" /></div>';
+    echo '<div class="form-group"><input type="checkbox" class="form-control" value="'.$row['id'].'" name="emailArray[]" /></div>';
     echo $row['first'] .' '. $row['last'] .' - '. $row['email'];
     echo '</label>';
 }
@@ -61,7 +62,7 @@ mysqli_close($dbconnection);
 ?>
 
 
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
                             </form>
                     </div>
                     <div class="col-md-4"></div>
